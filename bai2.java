185c85484e84fb823ba3048a835a17a55a77695c
tankiet712 <135820917+tankiet712@users.noreply.github.com>
2023-06-10 22:51:08 +0700

    Add files via upload

---
+++ bai2.java
@@ -0,0 +1,61 @@
+package bai2;
+
+public class bai2 {
+	private int maSV;
+    private String tenSV;
+    private float diemTL, diemTH;
+ 
+    //khởi tạo constructor không tham số
+    public bai2() {
+    }
+ 
+    //khởi tạo constructor có tham số
+    public bai2(int maSV, String tenSV, Float diemTL, Float diemTH) {
+        this.maSV = maSV;
+        this.tenSV = tenSV;
+        this.diemTH = diemTH;
+        this.diemTL = diemTL;
+    }
+ 
+    //begin getter and setter
+    public int getMaSV() {
+        return maSV;
+    }
+ 
+    public void setMaSV(int maSV) {
+        this.maSV = maSV;
+    }
+ 
+    public String getTenSV() {
+        return tenSV;
+    }
+ 
+    public void setTenSV(String tenSV) {
+        this.tenSV = tenSV;
+    }
+ 
+    public float getDiemTL() {
+        return diemTL;
+    }
+ 
+    public void setDiemTL(float diemTL) {
+        this.diemTL = diemTL;
+    }
+ 
+    public float getDiemTH() {
+        return diemTH;
+    }
+ 
+    public void setDiemTH(float diemTH) {
+        this.diemTH = diemTH;
+    }
+    //tạo hàm tính điểm trung bình
+    public float tinhDiemTB() {
+        return (diemTH + diemTL) / 2;
+    }
+ 
+    //sử dụng phương thức toString để hiển thị kết quả
+    public String toString() {
+        return maSV + " - " + tenSV + " diem Tb : " + tinhDiemTB();
+    }
+}
---
+++ main2.java
@@ -0,0 +1,11 @@
+package bai2;
+
+public class main2 {
+
+	public static void main(String[] args) {
+	bai2 sv1 = new bai2(8,9,21109841,"truongtankiet");
+	System.out.println(sv1().toString());
+
+	}
+
+}

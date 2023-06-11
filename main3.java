68a50ff49d18e26553f6ac770646f64bf35fba4b
tankiet712 <135820917+tankiet712@users.noreply.github.com>
2023-06-11 21:47:23 +0700

    Add files via upload

---
+++ main3.java
@@ -0,0 +1,19 @@
+package bai3;
+
+public class main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bai3 triangle1 = new bai3(3, 3, 3);
+		bai3 triangle2 = new bai3(3, 6, 18);
+		bai3 triangle3 = new bai3(3, 2, 5);
+		bai3 triangle4 = new bai3(5, 1, 4);
+		bai3 triangle5 = new bai3(5, 4, 5);
+		System.out.println(triangle1.toString());
+		System.out.println(triangle2.toString());
+		System.out.println(triangle3.toString());
+		System.out.println(triangle4.toString());
+		System.out.println(triangle5.toString());
+	}
+
+}
